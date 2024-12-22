3a303ae98886650bbee76bc15d393e18941922b4
ali sharifi <alisharifyofficial@gmail.com>
2024-12-22 22:27:19 +0330

    update default applications

--- docker/alpine/main.py
+++ docker/alpine/main.py
@@ -4,4 +4,4 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World from gunicorn-uvicorn-nginx"}
+    return {"Hello": "World from gunicorn-uvicorn-nginx", "msg": "test application, if you seeing this that means your not configure your dockerfile correctly."}
--- docker/main/main.py
+++ docker/main/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
+
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World from gunicorn-uvicorn-nginx"}
+    return {"Hello": "World from gunicorn-uvicorn-nginx", "msg": "test application, if you seeing this that means your not configure your dockerfile correctly."}
